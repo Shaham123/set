1005c7aac61c1d3201ba47d05ae828206808fc41
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-10-09 07:46:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,6 +34,7 @@ Here you can search files in Inline mode as well as PM, Use the below buttons to
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 RESULT_MSG = environ.get('RESULT_MSG', "")
+MAIN_GROUP = environ.get('MAIN_GROUP', "https://t.me/malllumoviesgroups")
 
 FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
 OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
